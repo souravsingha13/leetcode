fb4df313eea62bb5032b6d306956636a3cff97b8
souravsingha13 <69568511+souravsingha13@users.noreply.github.com>
2024-06-15 16:52:15 +0600

    Time: 40 ms (23.67%), Space: 16.5 MB (92.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,10 +3,11 @@ class Solution:
         stack = []
         lookup = {')': '(', '}': '{', ']': '['}
         for char in s:
-            if char in lookup:
+            if char in lookup: # The character is a closing bracket
+# Pop the top element from the stack if it's not empty; otherwise assign a dummy value
                 top_element=stack.pop() if stack else '#'
                 if lookup[char] != top_element:
                     return False
-            else:
+            else: # The character is an opening bracket
                 stack.append(char)
         return not stack
